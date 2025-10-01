825a71061da7dda88cb8e03909e4d893c04002d2
kiran <kothapallikiran541@gmail.com>
2025-10-01 17:30:27 +0530

    hello i sent two files

---
+++ int.py
@@ -0,0 +1,5 @@
+n1 = 13
+n2 = -5.5
+print (n1//n2)
+print(n1*n2)
+print ('hi',abs(n2))
---
+++ output.py
@@ -0,0 +1,11 @@
+
+# using formatted string (f) in print
+t1 , age ="kiran" , 28
+print(f"hi this is {t1} and i am just {age} old",'cool',sep=(" $ "))
+
+#using sep() and end() command in print                                        
+
+print ("kiran ",'vasu','ravi',sep=(' | '), end=(" "))
+print('are brothers')                             
+
+
